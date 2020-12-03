2c4cbcc56e516996c4564e9486ac67b38a8ea35a
federicavil <45661960+federicavil@users.noreply.github.com>
2020-12-03 09:11:29 +0100

    Collegamento con controller e schermo intero

--- PeakLand/src/view/graphicController/LoginGraphicController.java
+++ PeakLand/src/view/graphicController/LoginGraphicController.java
@@ -1,22 +1,31 @@
 package view.graphicController;
 
-import java.io.IOException;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class LoginGraphicController extends Application {
-	 	
-		@Override
-	    public void start(Stage primaryStage) throws IOException {
-	        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("view.ui.loginLayout.fxml")));
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	    }
+public class LoginGraphicController {
+	@FXML 
+	private Screen screen; 
+	
+	public void initialize() {
+	}
+	
+	public void setScreenDim(Stage stage) {
+		Rectangle2D screenBounds = screen.getVisualBounds();
+		//stage.setHeight(screenBounds.getHeight());
+		//stage.setWidth(screenBounds.getWidth());
+		stage.setMaximized(true);
+		//stage.setX(screenBounds.getMinX());
+		//stage.setY(screenBounds.getMinY());
+		/*GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		stage.setWidth(width);
+		stage.setHeight(height);
+		stage.setx
+		*/
+	}
 
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
 }
