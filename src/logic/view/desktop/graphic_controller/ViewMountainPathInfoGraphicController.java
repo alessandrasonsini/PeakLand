0d1f4917166c06e8a09852d9531e636a42462524
federicavil <45661960+federicavil@users.noreply.github.com>
2020-12-21 18:59:37 +0100

    Modify graph control

--- src/logic/view/desktop/graphic_controller/ViewMountainPathInfoGraphicController.java
+++ src/logic/view/desktop/graphic_controller/ViewMountainPathInfoGraphicController.java
@@ -1,31 +1,7 @@
 package logic.view.desktop.graphic_controller;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 
 public class ViewMountainPathInfoGraphicController {
 	
-	SearchMountainPathGraphicController searchController;
 	
-	public ViewMountainPathInfoGraphicController() {
-		// TODO Auto-generated constructor stub
-		searchController = new SearchMountainPathGraphicController();
-	}
-
-	@FXML
-	private TextField txtSearch;
-	
-	@FXML
-	private ImageView imgSearch; 
-	
-	@FXML
-	public HBox mainChild;
-	
-	@FXML
-    private void onSearchInputHandler(ActionEvent event) {
-        searchController.onSearchRequest(event);
-    }
 }
