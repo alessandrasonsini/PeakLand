a725e68fa2e4dcaee0cdf191ec0cfc32bce309fe
alessandrasonsini <56720833+alessandrasonsini@users.noreply.github.com>
2021-02-11 10:14:14 +0100

    Modifiche varie

---
+++ src/logic/model/utils/ReviewConverter.java
@@ -0,0 +1,31 @@
+package logic.model.utils;
+
+import logic.bean.ReviewBean;
+import logic.model.Review;
+
+public class ReviewConverter {
+
+	public static ReviewBean getReviewBean(Review review) {
+		ReviewBean bean = new ReviewBean();
+		
+		bean.setPathName(review.getPathName());
+		bean.setAuthor(review.getAuthor());
+		bean.setTitle(review.getTitle());
+		bean.setVote(review.getVote());
+		bean.setComment(review.getComment());
+		
+		return bean;
+	}
+	
+	public static Review getReview(ReviewBean bean) {
+		Review review = new Review();
+		
+		review.setPathName(bean.getPathName());
+		review.setAuthor(bean.getAuthor());
+		review.setTitle(bean.getTitle());
+		review.setVote(bean.getVote());
+		review.setComment(bean.getComment());
+		
+		return review;
+	}
+}
