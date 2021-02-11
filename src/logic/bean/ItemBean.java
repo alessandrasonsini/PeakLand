7ccc241d3728cea43d676fefd4eb46249736c212
alessandrasonsini <56720833+alessandrasonsini@users.noreply.github.com>
2021-02-11 10:00:26 +0100

    Modifiche varie

---
+++ src/logic/bean/ItemBean.java
@@ -0,0 +1,33 @@
+package logic.bean;
+
+/**
+ * Classe che rappresenta la generica bean
+ */
+public abstract class ItemBean {
+	
+	// Converte l'array di stringhe in un'unica stringa
+	public String convertToText(String[] array) {
+		String text = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			text = text.concat(" " + array[i]);
+		}
+		
+		return text;
+	}
+	
+	// Converte l'oggetto generico in stringa, gestendo correttamente il valore null
+	public String convertToText(Object genericObject) {
+		if(genericObject == null) {
+			return "Not available";
+		}
+		else {
+			return genericObject.toString()
+					.replace("[", "")
+					.replace("]", "")
+					.replace("true", "yes")
+					.replace("false", "no");
+		}
+	}
+
+}
