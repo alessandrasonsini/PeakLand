37878723e63bea815a2e071a69e4eb7e887c6061
alessandrasonsini <56720833+alessandrasonsini@users.noreply.github.com>
2021-02-11 10:13:00 +0100

    Modifiche varie

---
+++ src/logic/model/utils/LoggedUserConverter.java
@@ -0,0 +1,38 @@
+package logic.model.utils;
+
+import logic.bean.LoggedUserBean;
+import logic.model.LoggedUser;
+import logic.model.enums.UserLevel;
+
+public class LoggedUserConverter {
+	
+	public static LoggedUserBean getLoggedUserBean(LoggedUser user) {
+		LoggedUserBean bean = new LoggedUserBean();
+		bean.setName(user.getName());
+		bean.setSurname(user.getSurname());
+		bean.setLevel(user.getLevel().toString().toLowerCase());
+		bean.setDescription(user.getDescription());
+		
+		return bean;
+	}
+	
+	public static LoggedUserBean getLoggedUserBean(String name, String surname, String description) {
+		LoggedUserBean bean = new LoggedUserBean();
+		bean.setName(name);
+		bean.setSurname(surname);
+		bean.setDescription(description);
+		
+		return bean;
+	}
+	
+	public static LoggedUser getLoggedUser(LoggedUserBean bean) {
+		LoggedUser user = new LoggedUser();
+		user.setName(bean.getName());
+		user.setSurname(bean.getSurname());
+		user.setLevel(UserLevel.valueOf(bean.getLevel()));
+		user.setDescription(bean.getDescription());
+		
+		return user;
+	}
+	
+}
